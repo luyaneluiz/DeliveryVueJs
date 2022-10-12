e553c9fc84aee0c1c4830bf1edfc9c2cb63a731f
luyaneluiz <luyane.luiz.ll@gmail.com>
2022-10-12 15:44:07 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <Navbar :logo="logo_src" :alt="app_name" />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make your burger",
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Banner />
+    <div class="main-container">
+      <h1>HOME</h1>
+      <BurgerForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import Banner from "../components/Banner.vue";
+import BurgerForm from "../components/BurguerForm.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Banner,
+    BurgerForm,
+  },
+};
+</script>
